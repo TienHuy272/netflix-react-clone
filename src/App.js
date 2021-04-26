3c6dfb9ebd0126db4606783f5e45d479cf00a66f
Nguyen Tien Huy <tienhuy@Nguyens-MacBook-Pro.local>
2021-04-26 11:34:32 +0700

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Jumbotron from './components/jumbotron/index';
+import jumboData from '../src/fixtures/jumbo';
 
 function App() {
-  return <p>Hello</p>;
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
 }
 
 export default App;
